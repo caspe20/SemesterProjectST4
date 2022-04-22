926eb727b410566b6fa2d18bb91154a141c638d8
Casper K. Jensen <caspe20@student.sdu.dk>
2022-04-22 09:42:52 +0200

    Fully functional MQTT example implementation

--- prototype/src/main/java/AssemblyStation.java
+++ prototype/src/main/java/AssemblyStation.java
@@ -1,34 +1,48 @@
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-class AssemblyStation{
-    private MQTTAsyncListener actionListener = new MQTTAsyncListener("Test", "emulation/operation", "userContext");
+class AssemblyStation implements MqttCallback{
 
+    public static void main(String[] args) {
+        MqttClient client;
 
-    private class MQTTAsyncListener implements IMqttActionListener{
-        protected final String messageText;
-        protected final String topic;
-        protected final String userContext;
+        try {
+            client = new MqttClient("tcp://localhost:1883","1");
+            MqttConnectOptions options = new MqttConnectOptions();
 
-        private MQTTAsyncListener(String messageText, String topic, String userContext) {
-            this.messageText = messageText;
-            this.topic = topic;
-            this.userContext = userContext;
+//            options.setUserName("some name");
+//            options.setPassword(new char[]{1,2,3,4});
+
+            System.out.println("Connecting");
+            client.connect();
+            System.out.println("Connected");
+            System.out.println("Subscribing");
+            client.subscribe("emulator/status",1);
+            client.subscribe("emulator/status",(topic,msg)->{
+                System.out.println(topic + ": " + msg);
+            });
+            client.subscribe("emulator/checkhealth",(topic,msg)->{
+                System.out.println(topic + ": " + msg);
+            });
+            client.publish("emulator/operation",new MqttMessage("{ \"ProcessID\" : 1 }".getBytes()));
+            System.out.println("Subscribed");
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
+    }
 
-        @Override
-        public void onSuccess(IMqttToken asyncActionToken) {
-            if ((asyncActionToken != null) && asyncActionToken.getUserContext().equals(userContext)) {
-                System.out.printf("Message '%s' published to topic '%s'%n", messageText, topic);
-            }
-        }
+    @Override
+    public void connectionLost(Throwable throwable) {
+        System.out.println("yikes");
+    }
 
-        @Override
-        public void onFailure(
-                IMqttToken asyncActionToken,
-                Throwable exception) {
-            exception.printStackTrace();
-        }
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+        System.out.printf("Message '%s' received on topic '%s'", mqttMessage,s);
+    }
 
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+        System.out.println("I did a good job!");
     }
 }
