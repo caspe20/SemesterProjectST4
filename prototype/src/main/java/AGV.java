aaa96cb8022ae3ff51640107f43199055d2e715a
Mette Hansen <meha419@student.sdu.dk>
2022-04-22 10:34:32 +0200

    Http verb GET functional + JSON values

--- prototype/src/main/java/AGV.java
+++ prototype/src/main/java/AGV.java
@@ -1,2 +1,32 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class AGV {
+
+    public static void main(String[] args) throws JsonProcessingException {
+        //Access to HTTP response
+        RestTemplate restTemplate = new RestTemplate();
+        String resourceURL = "http://localhost:8082";
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(resourceURL + "/v1/status/", String.class);
+        Assertions.assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+        //System.out.println(responseEntity);
+
+        //Get JSON values
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseEntity.getBody());
+
+        JsonNode battery = root.path("battery");
+        JsonNode program_name = root.path("program name");
+        JsonNode state = root.path("state");
+        JsonNode timeStamp = root.path("timestamp");
+        System.out.println(battery + " " + program_name + " " + state + " " + timeStamp);
+
+    }
+
 }
