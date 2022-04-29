30b946c35ec6b9c48bec6622c4ffd772cb928844
Line Andersen <linea.89@outlook.dk>
2022-04-29 15:03:16 +0200

    GET methods working

--- prototype/src/main/java/example/AGVClient.java
+++ prototype/src/main/java/example/AGVClient.java
@@ -1,6 +1,12 @@
 package example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -9,9 +15,17 @@ public class AGVClient {
     private final String resourceUrl = "http://localhost:8082/v1/status/";
     private RestTemplate restTemplate = new RestTemplate();
 
-    public AGVClient() {
+    private String resourceURL1 = "http://localhost:8082";
+    ResponseEntity<String> responseEntity = restTemplate.getForEntity(resourceURL1 + "/v1/status/", String.class);
+
+
+    private ObjectMapper mapper = new ObjectMapper();
+    private JsonNode root = mapper.readTree(responseEntity.getBody());
+
+    public AGVClient() throws JsonProcessingException {
     }
 
+    //Methods for Http PUT
     public void moveToAssemblyStation() {
         HashMap<String, Object> request = new HashMap<>();
         request.put("Program name", "MoveToAssemblyOperation");
@@ -68,17 +82,33 @@ public class AGVClient {
         execute();
     }
 
-
+    // Method for executing Http PUT
     public void execute() {
         JSONObject executeTest = new JSONObject();
         executeTest.put("State", 2);
         restTemplate.put(resourceUrl, executeTest);
     }
 
-    public static void main(String[] args) {
-        AGVClient try1 = new AGVClient();
-        try1.moveToChargerOperation();
-
+    public JsonNode getBattery(){
+        JsonNode battery = root.path("battery");
+        return battery;
     }
+
+    public JsonNode getProgram_name(){
+        JsonNode program_name = root.path("program name");
+        return program_name;
+    }
+
+    public JsonNode getState(){
+        JsonNode state = root.path("state");
+        return state;
+    }
+
+    public JsonNode getTimeStamp(){
+        JsonNode timeStamp = root.path("timestamp");
+        return timeStamp;
+    }
+
+    
 
 }
