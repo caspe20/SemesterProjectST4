0bd5fa50dcc971b7c52dab4e675ecece2051b7ce
Mette Hansen <meha419@student.sdu.dk>
2022-05-12 10:34:23 +0200

    Go nuts Line

--- application/DroneFactory/AGV/src/main/java/com/sdu/main/AGV.java
+++ application/DroneFactory/AGV/src/main/java/com/sdu/main/AGV.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.minidev.json.JSONObject;
-import org.junit.jupiter.api.Assertions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
